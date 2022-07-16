2d6015ce0d3525aa18a1fead5405dbc4f5037b6b
vagrant9527 <45305214+vagrant9527@users.noreply.github.com>
2022-07-16 23:01:49 +0800

    flask,路由

--- demos/hello/app.py
+++ demos/hello/app.py
@@ -1,7 +1,23 @@
 from flask import Flask
 
 app=Flask(__name__)
+#从flask包导入Flask类
+#实例化这个类，就得到程序实例app
 
+#注册路由
 @app.route('/')
 def index():
     return '<h1>Hello Flask!</h1>'
+
+#为视图绑定多个路由
+@app.route('/hi')
+@app.route('/hello')
+def say_hello():
+    return '<h1>Hello Flask!!!</h1>'
+
+#动态URL
+@app.route('/greet/<name>')
+@ap.route('/greet',defaults={'name':'Programmer'})
+def greet(name):
+    return '<h1>Hello,%s!</h1>' % name
+
